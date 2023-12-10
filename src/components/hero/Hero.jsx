34692a988bfc0c9dfd135f87025665c8f3323163
taahahussainkhan <taaha128@gmail.com>
2023-12-10 17:33:26 +0500

    Hero section in progress

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import './hero.scss';
+import React from "react";
+import "./hero.scss";
 
 export const Hero = () => {
   return (
-    <div className='hero'>
-        <div className="imageContainer">
-            <img src="/hero.png" alt="" srcset="" />
+    <div className="hero">
+      <div className="wrapper">
+        <div className="textContainer">
+          <h2>TAAHA HUSSAIN KHAN</h2>
+          <h1>Full Stack Web Developer</h1>
+          <div className="buttons">
+            <button>See the latest works</button>
+            <button>Contact Me</button>
+          </div>
+          <img src="/scroll.png" alt="" srcset="" />
         </div>
+      </div>
+      <div className="imageContainer">
+        <img src="/hero.png" alt="" srcset="" />
+      </div>
     </div>
-  )
-}
+  );
+};
