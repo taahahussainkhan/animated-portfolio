e7942628abe928f875da93aee16d1514280b33ae
taahahussainkhan <taaha128@gmail.com>
2023-12-19 00:12:59 +0500

    Parallax

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,19 +1,34 @@
 import React from 'react'
 import './parallax.scss'
+import { motion , useScroll, useTransform} from 'framer-motion'
 
 
 export const Parallax = (props) => {
+
+const ref = useRef()
+
+  const {scrollYProgress} = useScroll({
+    target:ref,
+    offset:["start start","end start"]
+  });
+
+const yBG = useTransform(scrollYProgress, [0,1], [0, 100]);
+
   return (
-    <div className='parallax' style={{
+    <div className='parallax' 
+    ref={ref}
+    style={{
       background:
       props.type === "services" ? 
       "linear-gradient(180deg, #111132, #0c0c1d)":
        "linear-gradient(180deg, #111132, #505064)"
     }}>
-      <h1>{props.type === "services" ? "What We Do?" : "What We Did?"}</h1>
-      <div className="mountains"></div>
-      <div className="planets"></div>
-      <div className="stars"></div>
+      <motion.h1
+      style={{y: yBG}}
+      >{props.type === "services" ? "What We Do?" : "What We Did?"}</motion.h1>
+      <motion.div className="mountains"></motion.div>
+      <motion.div className="planets"></motion.div>
+      <motion.div className="stars"></motion.div>
     </div>
   )
 }
